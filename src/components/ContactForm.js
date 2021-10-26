832d1af8ad816f43521bd53f0afefcea402316c9
bloic <loic.benos@gmail.com>
2021-10-26 20:06:15 +0200

    ajoute de la sécurisation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,13 +2,19 @@ import React from 'react';
 import {Formik, Form} from 'formik';
 import * as Yup from 'yup';
 
+const errorsMessage = "ce champ est requis";
 
 const FormSchema = Yup.object().shape({
-    mail: Yup.string().email('invalid email').required('required'),
+    mail: Yup.string()
+        .email('invalid email')
+        .required(errorsMessage),
     objet: Yup.string()
         .min(5, 'trop court')
         .max(30, 'trop long')
-        .required('required'),
+        .required(errorsMessage),
+    message: Yup.string()
+        .min(10, "Vous devez saisir 10 caractères minimun.")
+        .required(errorsMessage),
 
 })
 
@@ -26,22 +32,24 @@ const ContactForm = () => {
             }}>
 
             {({errors, touched}) => (
-                <Form method="POST" className="form">
+                <Form method="POST"  className="form">
                     <div>
                         <label htmlFor="mail">Mail</label>
                         <input type="text" id="mail" placeholder="e-mail" name="mail"/>
-                        {errors.mail && touched.mail ? <span>{errors.mail}</span> : null}
+                        {errors.mail && touched.mail ? <span className="errorsMessage">{errors.mail}</span> : null}
                     </div>
 
                     <div>
                         <label htmlFor="objet">Objet</label>
                         <input type="text" id="objet" placeholder="Objet" name="Objet"/>
-                        {errors.objet && touched.objet ? <span>{errors.objet}</span> : null}
+                        {errors.objet && touched.objet ? <span className="errorsMessage">{errors.objet}</span> : null}
                     </div>
 
                     <div>
                         <label htmlFor="message">Message</label>
                         <textarea name="message" id="message" placeholder="Entrez votre message" cols="30" rows="10"/>
+                        {errors.message && touched.message ? <span className="errorsMessage">{errors.message}</span> : null}
+
                     </div>
                     <button type="submit" className="btn submit">Envoyer</button>
                 </Form>
