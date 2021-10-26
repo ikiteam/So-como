b79d85897d57b6f7e6eb1a2835e0151af5655fa1
bloic <loic.benos@gmail.com>
2021-10-26 19:41:57 +0200

    sécurisation formulaire de contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,26 +1,53 @@
+import React from 'react';
+import {Formik, Form} from 'formik';
+import * as Yup from 'yup';
+
+
+const FormSchema = Yup.object().shape({
+    mail: Yup.string().email('invalid email').required('required'),
+    objet: Yup.string()
+        .min(5, 'trop court')
+        .max(30, 'trop long')
+        .required('required'),
+
+})
+
 
 const ContactForm = () => {
+    return (
+        <Formik
+            initialValues={{
+                mail: '',
+                objet: '',
+            }}
+            validationSchema={FormSchema}
+            onSubmit={values => {
+                console.log(values);
+            }}>
 
-    return(
+            {({errors, touched}) => (
+                <Form method="POST" className="form">
+                    <div>
+                        <label htmlFor="mail">Mail</label>
+                        <input type="text" id="mail" placeholder="e-mail" name="mail"/>
+                        {errors.mail && touched.mail ? <span>{errors.mail}</span> : null}
+                    </div>
 
-        <form action="#" method="POST" className="form">
-            
-            <div>
-                <label htmlFor="mail">Mail</label>
-                <input type="text" id="mail" placeholder="Mail" />
-            </div>
+                    <div>
+                        <label htmlFor="objet">Objet</label>
+                        <input type="text" id="objet" placeholder="Objet" name="Objet"/>
+                        {errors.objet && touched.objet ? <span>{errors.objet}</span> : null}
+                    </div>
 
-            <div>
-                <label htmlFor="objet">Objet</label>
-                <input type="text" id="objet" placeholder="Objet"/>
-            </div>
+                    <div>
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" placeholder="Entrez votre message" cols="30" rows="10"/>
+                    </div>
+                    <button type="submit" className="btn submit">Envoyer</button>
+                </Form>
+            )}
 
-            <div>
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" placeholder="Entrez votre message"  cols="30" rows="10"/>
-            </div>
-            <a href="#" className="btn submit">Envoyer</a>
-        </form>
+        </Formik>
     )
 
 }
