988af8b64b994489c40250e1b1cddbfacaf5f018
bloic <loic.benos@gmail.com>
2021-11-04 21:30:23 +0100

    resolution de la validation du formulaire de contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,62 +1,65 @@
 import React from 'react';
-import {Formik, Form} from 'formik';
 import * as Yup from 'yup';
+import {useFormik} from 'formik';
 
-const errorsMessage = "ce champ est requis";
-
-const FormSchema = Yup.object().shape({
-    mail: Yup.string()
-        .email('invalid email')
-        .required(errorsMessage),
-    objet: Yup.string()
-        .min(5, 'trop court')
-        .max(30, 'trop long')
-        .required(errorsMessage),
-    message: Yup.string()
-        .min(10, "Vous devez saisir 10 caractères minimun.")
-        .required(errorsMessage),
-
-})
 
 
 const ContactForm = () => {
+    const errorsMessage = "ce champ est requis";
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            object: '',
+            message: '',
+        },
+        validationSchema: Yup.object().shape({
+            email: Yup.string()
+                .email('l\'email est invalide.')
+                .required(errorsMessage),
+            object: Yup.string()
+                .min(5, 'l\'objet doit faire 5 caractères minimun.')
+                .max(30, 'l\'objet doit faire moin de 30 caractères.')
+                .required(errorsMessage),
+            message: Yup.string()
+                .min(10, "Vous devez saisir 10 caractères minimun.")
+                .required(errorsMessage),
+
+        }),
+        onSubmit: values => {
+            console.log(values)
+        },
+    });
     return (
-        <Formik
-            initialValues={{
-                mail: '',
-                objet: '',
-            }}
-            validationSchema={FormSchema}
-            onSubmit={values => {
-                console.log(values);
-            }}>
+        <form action="#" method="post" onSubmit={formik.handleSubmit} className="form">
+            <div>
+                <label htmlFor="email">Mail</label>
+                {formik.touched.email && formik.errors.email ? (
+                    <span className="errorsMessage">{formik.errors.email}</span>) : null}
+                <input type="text" id="email" placeholder="e-mail" name="email" {...formik.getFieldProps('email')}/>
+            </div>
 
-            {({errors, touched}) => (
-                <Form method="POST"  className="form">
-                    <div>
-                        <label htmlFor="mail">Mail</label>
-                        {errors.mail && touched.mail ? <span className="errorsMessage">{errors.mail}</span> : null}
-                        <input type="text" id="mail" placeholder="e-mail" name="mail"/>
-                    </div>
+            <div>
+                <label htmlFor="object">Objet</label>
+                {formik.touched.object && formik.errors.object ? (
+                    <span className="errorsMessage">{formik.errors.object}</span>) : null}
+                <input type="text" id="object" placeholder="Objet"
+                       name="object" {...formik.getFieldProps('object')}/>
+            </div>
 
-                    <div>
-                        <label htmlFor="objet">Objet</label>
-                        {errors.objet && touched.objet ? <span className="errorsMessage">{errors.objet}</span> : null}
-                        <input type="text" id="objet" placeholder="Objet" name="Objet"/>
-                    </div>
+            <div>
+                <label htmlFor="message">Message</label>
+                {formik.touched.message && formik.errors.message ? (
+                    <span className="errorsMessage">{formik.errors.message}</span>) : null}
+                <textarea name="message" id="message" placeholder="Entrez votre message" cols="30" rows="10"
+                          {...formik.getFieldProps('message')}/>
 
-                    <div>
-                        <label htmlFor="message">Message</label>
-                        {errors.message && touched.message ? <span className="errorsMessage">{errors.message}</span> : null}
-                        <textarea name="message" id="message" placeholder="Entrez votre message" cols="30" rows="10"/>
+            </div>
+            <button type="submit" className="btn submit">Envoyer</button>
+        </form>
+    );
 
-                    </div>
-                    <button type="submit" className="btn submit">Envoyer</button>
-                </Form>
-            )}
+};
 
-        </Formik>
-    )
 
-}
-export default ContactForm
+export default ContactForm;
