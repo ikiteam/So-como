83e10ef02d3d3e84c44e43bcc352b9f8856e865e
bloic <loic.benos@gmail.com>
2021-12-05 18:54:21 +0100

    possible resolution recaptcha

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,6 +12,7 @@ import Recaptcha from "react-google-recaptcha";
 const ContactForm = () => {
     const errorsMessage = "ce champ est requis";
     const form = useRef();
+
     const validation = Yup.object().shape({
         email: Yup.string()
             .email('l\'email est invalide.')
@@ -33,7 +34,7 @@ const ContactForm = () => {
                 message: "",
             }}
             validationSchema={validation}
-            onSubmit={ values => {
+            onSubmit={values => {
                 console.log(values);
 
                 if (values !== "") {
@@ -51,51 +52,62 @@ const ContactForm = () => {
                 }
             }
             }
+
         >
             {({
-                         values, errors, touched,
-                         handleSubmit, setFieldValue
-                     }) => (
-            <Form className="form" ref={form} onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">email</label>
-                    <Field name="email" placeholder="e-mail" />
-                    {errors.email && touched.email ? (
-                        <div className="errorsMessage">{errors.email}</div>
-                    ) : null}
-                </div>
+                  values, errors, touched,
+                  handleSubmit, setFieldValue
+              }) => (
+                <Form className="form" ref={form} onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">email</label>
+                        <Field name="email" placeholder="e-mail"/>
+                        {errors.email && touched.email ? (
+                            <div className="errorsMessage">{errors.email}</div>
+                        ) : null}
+                    </div>
 
-                <div>
-                    <label htmlFor="object">objet</label>
-                    <Field name="object"  placeholder="objet" />
-                    {errors.object && touched.object ? (
-                        <div className="errorsMessage">{errors.object}</div>
-                    ) : null}
-                </div>
+                    <div>
+                        <label htmlFor="object">objet</label>
+                        <Field name="object" placeholder="objet"/>
+                        {errors.object && touched.object ? (
+                            <div className="errorsMessage">{errors.object}</div>
+                        ) : null}
+                    </div>
 
-                <div>
-                    <label htmlFor="message">message</label>
-                    <Field name="message" as="textarea" />
-                    {errors.message && touched.message ? (
-                        <div className="errorsMessage">{errors.message}</div>
-                    ) : null}
-                </div>
-                <div>
-                    <Recaptcha
-                        sitekey="6LeyHDAdAAAAAAd_-TqmfDNu6CsPNXQ_u9mb-2rL"
-                        render="explicit"
-                        verifyCallback={(response) => { setFieldValue("Recaptcha", response); }}
-                    />
-                    {errors.recaptcha
-                    && touched.recaptcha && (
-                        <div className="errorsMessage">{errors.recaptcha}</div>
-                    )}
-                    <button type="submit" id="btnSend" className="btn submit">Envoyer</button>
-                </div>
-            </Form>
-        )}
-        </Formik>
-    );
-};
+                    <div>
+                        <label htmlFor="message">message</label>
+                        <Field name="message" as="textarea"/>
+                        {errors.message && touched.message ? (
+                            <div className="errorsMessage">{errors.message}</div>
+                        ) : null}
+                    </div>
+                    <div>
+                        <Recaptcha
+                            sitekey="6LeyHDAdAAAAAAd_-TqmfDNu6CsPNXQ_u9mb-2rL"
+                            render="explicit"
+                            verifyCallback={(response) => {
+                                setFieldValue("Recaptcha", response);
+                            }}
+                            validationOnChange={values => {
+                                if (values !== "") {
+                                document.getElementById("btnSend").disabled = false;
+                            } else {
+                                document.getElementById("btnSend").disabled = true;
+                            }
+                            }
+                            }
+                                />
+                            {errors.recaptcha
+                                && touched.recaptcha && (
+                                <div className="errorsMessage">{errors.recaptcha}</div>
+                                )}
+                                <button type="submit" id="btnSend" className="btn submit">Envoyer</button>
+                                </div>
+                                </Form>
+                                )}
+                    </Formik>
+                    );
+                    };
 
-export default ContactForm;
+                    export default ContactForm;
